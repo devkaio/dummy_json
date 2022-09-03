8052f68e05cdcd9044fe8445d8957bd70c0548bd
Kaio C. de Oliveira <hello@kaio.dev>
2022-09-03 17:41:02 -0300

    add home_state abstract and concrete classes

---
+++ lib/src/features/home/state/home_state.dart
@@ -0,0 +1,9 @@
+abstract class HomeState {}
+
+class HomeStateEmpty implements HomeState {}
+
+class HomeStateLoading implements HomeState {}
+
+class HomeStateSuccess implements HomeState {}
+
+class HomeStateError implements HomeState {}
