11ab566f199d29b2f8d13e2c7beb029062cc8b31
Kaio C. de Oliveira <hello@kaio.dev>
2022-09-04 14:34:08 -0300

    add named_route constant

---
+++ lib/src/shared/constants/constants.dart
@@ -0,0 +1,1 @@
+export 'named_route.dart';
---
+++ lib/src/shared/constants/named_route.dart
@@ -0,0 +1,7 @@
+class NamedRoute {
+  static const String start = "/";
+  static const String home = "/home";
+  static const String register = "/register";
+  static const String login = "/login";
+  static const String recoverPassword = "/recover_password";
+}
